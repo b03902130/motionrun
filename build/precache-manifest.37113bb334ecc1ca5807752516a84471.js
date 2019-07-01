6fbafe69efb3d7e2b022a67a03babb19ffa7c7fa
Leo Yang <leo19941227@gmail.com>
2019-07-02 01:36:07 +0800

    generate static file

---
+++ build/precache-manifest.37113bb334ecc1ca5807752516a84471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880e1be078b1ffe718894690d9b73e3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15670cd7ec3f2c59e454",
+    "url": "/static/css/main.eb645909.chunk.css"
+  },
+  {
+    "revision": "4958e7aab4d0534413b0",
+    "url": "/static/js/2.04171a72.chunk.js"
+  },
+  {
+    "revision": "15670cd7ec3f2c59e454",
+    "url": "/static/js/main.103eb32c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d7fd9e0b952d5f9b9adff6ec29a8b20d",
+    "url": "/static/media/boy.d7fd9e0b.png"
+  }
+]);
